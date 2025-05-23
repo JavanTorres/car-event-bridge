b6a1f073fcde11570558c3911b8d9364daab520d
JavanTorres <javantorres@gmail.com>
2025-05-23 19:44:09 -0300

    chore: ajustes no chassi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
+import { existsSync } from 'node:fs';
+
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { RoutesV1Module } from './modules/routes-v1.module';
 
+const nodeEnv = process.env.NODE_ENV?.toLowerCase() || '';
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), RoutesV1Module],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env.${nodeEnv}`, '.env'].filter((path) =>
+        existsSync(path),
+      ),
+    }),
+    RoutesV1Module,
+  ],
 })
 export class AppModule {}
